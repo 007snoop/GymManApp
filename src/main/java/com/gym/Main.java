b3dd157b81c4e382bac22eb93b3e3acaf588e7aa
Colin Yetman <colinguitarist@gmail.com>
2025-04-09 13:20:58 -0230

    DB connection psql success

--- src/main/java/com/gym/Main.java
+++ src/main/java/com/gym/Main.java
@@ -1,4 +1,21 @@
 package com.gym;
 
+import com.gym.util.DBUtil;
+
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DBUtil.getConnection();
+            if (connection != null ) {
+                System.out.println("Connected to PSQL");
+            } else {
+                System.out.println("PSQL CONNECTION ERROR");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ src/main/java/com/gym/util/DBUtil.java
@@ -0,0 +1,31 @@
+package com.gym.util;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+
+        try {
+            Dotenv dotenv = Dotenv.load();
+
+            String url = dotenv.get("DB_URL");
+            String username = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+
+            assert url != null;
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
