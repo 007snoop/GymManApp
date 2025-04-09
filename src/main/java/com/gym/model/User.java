d0029ccb71c7079592a78068bc088bda51c7f450
Colin Yetman <colinguitarist@gmail.com>
2025-04-09 14:11:23 -0230

    update user.java -add constructor for user class

--- src/main/java/com/gym/model/User.java
+++ src/main/java/com/gym/model/User.java
@@ -1,4 +1,53 @@
 package com.gym.model;
 
 public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String role;
+
+    // constructor
+    public User(int userId, String username, String password, String email, String phoneNumber, String address,
+                String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+
+    // getter and setter
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
