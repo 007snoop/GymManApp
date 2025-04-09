86b7589a9f0d434e30e6759cf2297472b3277ee5
Colin Yetman <colinguitarist@gmail.com>
2025-04-09 14:39:48 -0230

    user service update -add user registration method -add auth to bcrypt pass hash

--- src/main/java/com/gym/service/UserService.java
+++ src/main/java/com/gym/service/UserService.java
@@ -1,4 +1,39 @@
 package com.gym.service;
 
+import com.gym.dao.UserDAO;
+import com.gym.model.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class UserService {
+    private UserDAO userDAO;
+
+    public UserService(Connection connection) {
+        this.userDAO = new UserDAO(connection);
+    }
+    // make user with hashed pass
+    public void register(User user) throws SQLException {
+        String hashedPass = hashPassword(user.getPassword());
+        user = new User(user.getUserId(), user.getUsername(), hashedPass, user.getEmail(), user.getPhoneNumber(),
+                user.getAddress(), user.getRole());
+        userDAO.registerUser(user);
+    }
+
+    // hash the pass
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    // verify pass on login
+    public boolean verifyPass(String inputPassword, String storedHashedPassword) {
+        return BCrypt.checkpw(inputPassword, storedHashedPassword);
+    }
+
+    // get user by username
+    public User getUserByUsername(String username) throws SQLException {
+        return userDAO.getUserByUsername(username);
+    }
+
 }
