93cb5e922752e965972ff8befc5d1e14f82e5d64
Colin Yetman <colinguitarist@gmail.com>
2025-04-09 14:28:35 -0230

    data access object update - add method to handle user regitration - add method to get users registered

--- src/main/java/com/gym/dao/UserDAO.java
+++ src/main/java/com/gym/dao/UserDAO.java
@@ -1,4 +1,67 @@
 package com.gym.dao;
 
+import com.gym.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
+    private Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // register a new user
+    public void registerUser(User user) throws SQLException {
+        String SQL = """
+                INSERT INTO users (
+                username,
+                password,
+                email,
+                phone_number,
+                address,
+                role
+                ) VALUES (?,?,?,?,?,?)""";
+        try (PreparedStatement stmt = connection.prepareStatement(SQL)) {
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getPhoneNumber());
+            stmt.setString(5, user.getAddress());
+            stmt.setString(6, user.getRole());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // get user by username
+    public User getUserByUsername(String username) throws SQLException {
+        String SQL = """
+                SELECT * FROM
+                users
+                WHERE
+                username = ?;""";
+        try (PreparedStatement stmt = connection.prepareStatement(SQL)) {
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new User(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"),
+                        rs.getString("phone_number"),
+                        rs.getString("address"),
+                        rs.getString("role")
+                );
+            }
+        }
+        return null;
+
+
+    }
 }
