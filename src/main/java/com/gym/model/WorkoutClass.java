df58896bc4471b591bd5b606e1a18fdb8a61a087
Colin Yetman <colinguitarist@gmail.com>
2025-04-12 17:52:26 -0230

    workout class - add workout class - add contrustor - add getters and setters

--- src/main/java/com/gym/model/WorkoutClass.java
+++ src/main/java/com/gym/model/WorkoutClass.java
@@ -1,4 +1,47 @@
 package com.gym.model;
 
 public class WorkoutClass {
+    private int workoutClassId;
+    private String workoutClassType;
+    private String workoutClassDesc;
+    private int trainerId;
+
+    public WorkoutClass(int workoutClassId, String workoutClassType, String workoutClassDesc, int trainerId) {
+        this.workoutClassId = workoutClassId;
+        this.workoutClassType = workoutClassType;
+        this.workoutClassDesc = workoutClassDesc;
+        this.trainerId = trainerId;
+    }
+
+    public int getWorkoutClassId() {
+        return workoutClassId;
+    }
+
+    public void setWorkoutClassId(int workoutClassId) {
+        this.workoutClassId = workoutClassId;
+    }
+
+    public String getWorkoutClassType() {
+        return workoutClassType;
+    }
+
+    public void setWorkoutClassType(String workoutClassType) {
+        this.workoutClassType = workoutClassType;
+    }
+
+    public String getWorkoutClassDesc() {
+        return workoutClassDesc;
+    }
+
+    public void setWorkoutClassDesc(String workoutClassDesc) {
+        this.workoutClassDesc = workoutClassDesc;
+    }
+
+    public int getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
 }
