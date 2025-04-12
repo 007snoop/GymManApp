a6e4c440b38cd79c6519ae4a3f18c5364f3e8ff1
Colin Yetman <colinguitarist@gmail.com>
2025-04-12 18:09:29 -0230

    data access object update - update workoutclass dao to connection and retrieve db table

--- src/main/java/com/gym/dao/WorkoutClassDAO.java
+++ src/main/java/com/gym/dao/WorkoutClassDAO.java
@@ -1,4 +1,53 @@
 package com.gym.dao;
 
+import com.gym.model.WorkoutClass;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorkoutClassDAO {
+    private final Connection connection;
+
+    public WorkoutClassDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void addWorkoutClass(WorkoutClass wc) throws SQLException {
+        String SQL = """
+                INSERT INTO workout_classes (
+                workout_class_type, workout_class_desc, trainer_id
+                ) VALUES (?,?,?)""";
+
+        try (PreparedStatement stmt = connection.prepareStatement(SQL)) {
+            stmt.setString(1, wc.getWorkoutClassType());
+            stmt.setString(2, wc.getWorkoutClassDesc());
+            stmt.setInt(3, wc.getTrainerId());
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<WorkoutClass> getAllWorkoutClasses() throws SQLException {
+        List<WorkoutClass> classes = new ArrayList<>();
+        String SQL = """
+                SELECT *
+                FROM
+                workout_classes""";
+
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            while (rs.next()) {
+                WorkoutClass wc = new WorkoutClass(
+                        rs.getInt("workout_class_id"),
+                        rs.getString("workout_class_type"),
+                        rs.getString("workout_class_desc"),
+                        rs.getInt("trainer_id")
+                );
+                classes.add(wc);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
 }
