08cbbb016937ffbfe880db361fdef67200758c53
Colin Yetman <colinguitarist@gmail.com>
2025-04-12 18:37:07 -0230

    add workout class service class

--- src/main/java/com/gym/service/WorkoutClassService.java
+++ src/main/java/com/gym/service/WorkoutClassService.java
@@ -1,4 +1,24 @@
 package com.gym.service;
 
+import com.gym.dao.WorkoutClassDAO;
+import com.gym.model.WorkoutClass;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class WorkoutClassService {
+    private final WorkoutClassDAO workoutClassDAO;
+
+    public WorkoutClassService(Connection connection) {
+        this.workoutClassDAO = new WorkoutClassDAO(connection);
+    }
+
+    public void addWorkoutclass(WorkoutClass workoutClass) throws SQLException {
+        workoutClassDAO.addWorkoutClass(workoutClass);
+    }
+
+    public List<WorkoutClass> getAllWorkoutClasses() throws SQLException {
+        return workoutClassDAO.getAllWorkoutClasses();
+    }
 }
